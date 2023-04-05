ca2c890660b1c0c1a7ef692c392511a04e8728b8
LolaLauman <lolalauman547@gmail.com>
2023-04-05 20:11:27 -0300

    captura de eventos 2

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import { Button } from "react-bootstrap";
+import './App.css';
+
+function App() {
+
+  return (
+
+    <div>
+      <center>
+        <h1>Contar vocales</h1>
+      </center>
+      <hr></hr>
+      <form onSubmit={vocales}>
+        <center>
+          <p>Ingrese palabra:
+            <input className='separar' type="text" name="texto">
+            </input>
+            <hr></hr>
+          </p>
+          <p>
+            <Button variant="success">Contar</Button>{ }
+          </p>
+        </center>
+      </form>
+    </div>
+  );
+}
+
+function vocales(e) {
+  e.preventDefault();
+  const texto = e.target.texto.value;
+  const vocales = 'AEIOUaeiou';
+  let contador = 0;
+  for (let i = 0; i < texto.length; i++) {
+    if (vocales.includes(texto[i])) contador++;
+  }
+  alert("Cantidad de vocales: " + contador);
+
+}
+export default App;
+
